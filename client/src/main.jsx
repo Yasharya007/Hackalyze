fd44c929e6f0210a86c2ef7a5b1ac3e9f34004d3
Yash Arya <yasharya82003@gmail.com>
2025-03-22 23:34:48 +0530

    Redux setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import  {BrowserRouter} from 'react-router-dom'
 import {Toaster} from "react-hot-toast"
+import { Provider } from "react-redux";
+import store from './store.js';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    <Toaster/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster/>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
