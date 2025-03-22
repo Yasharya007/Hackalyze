e64f99fa4477da1fd072854dda38765d7ed5e04b
Trishita <trishu.kesarwani06@gmail.com>
2025-03-22 14:59:12 +0530

    refresh token added

--- server/models/Admin.models.js
+++ server/models/Admin.models.js
@@ -18,6 +18,10 @@ const AdminSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    refreshToken: {
+        type: String,
+        default: ""
+    },
     managedHackathons: [{ 
         type: mongoose.Schema.Types.ObjectId, 
         ref: "Hackathon" 
