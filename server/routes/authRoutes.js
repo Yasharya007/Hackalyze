2f40e76e260efcacbc730291db18e0f47e904a44
kratika125 <kratikabhadauria21@gmail.com>
2025-03-24 03:34:16 +0530

    Fix authRouter

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {loginUser,registerTeacher,registerStudent,logoutUser,registerAdmin } from "../controllers/authController.js";
+import {loginUser,registerTeacher,registerStudent,logoutUser } from "../controllers/authController.js";
 import { verifyUser } from "../middleware/auth.js";
 
 const router = express.Router();
