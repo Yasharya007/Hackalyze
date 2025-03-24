9c63e376ea599df3027a591e2f3cb30305ee5134
Akriti Gaur <akritigaur1990@gmail.com>
2025-03-25 03:24:15 +0530

    edit review routes

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { moveToReviewFolder, getReviewSubmissions, removeFromReviewFolder } from "../controllers/reviewController.js";
+import { markSubmissionAsReviewed, getReviewSubmissions, markSubmissionAsPending } from "../controllers/reviewController.js";
 
 const router = express.Router();
 
 // Move a submission to review folder
-router.put("/:submissionId/review", moveToReviewFolder);
+router.put("/:submissionId/review", markSubmissionAsReviewed);
 
 // Get all submissions in the review folder
 router.get("/review-folder", getReviewSubmissions);
 
 // Remove submission from review folder
-router.put("/:submissionId/remove-review", removeFromReviewFolder);
+router.put("/:submissionId/remove-review", markSubmissionAsPending);
 
 export default router;
