9dff8b5b965fb94e23de64c52f26d19630fc428a
Samudraneel Sarkar <samudraneel05@gmail.com>
2025-03-27 05:21:07 +0530

    logo appearing in all

--- client/src/pages/EnrolledHackathons.jsx
+++ client/src/pages/EnrolledHackathons.jsx
@@ -87,7 +87,10 @@ const EnrolledHackathons = () => {
                 <div>
                     {/* Header/Logo Section */}
                     <div className="p-6">
-                        <h1 className="text-2xl font-bold">Hackalyze</h1>
+                        <div className="flex items-center">
+                            <img src="/logo.png" alt="Hackalyze Logo" className="h-8 mr-2" />
+                            <h1 className="text-2xl font-bold">Hackalyze</h1>
+                        </div>
                     </div>
 
                     {/* Navigation */}
